fda615bfd11b65e9b38b94135d6a1e8dc0df8578
zubonyai.gellert <gellert.zubonyai@stud.uni-corvinus.hu>
2022-10-17 14:32:31 +0200

    3. fealadat

--- gyak6/gyak6/Form1.cs
+++ gyak6/gyak6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak6.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,21 @@ namespace gyak6
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
+        }
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
+
+    
 }
