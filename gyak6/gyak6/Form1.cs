d0843ce729322b8e564aa8440a25dcabe0e24ac1
zubonyai.gellert <gellert.zubonyai@stud.uni-corvinus.hu>
2022-10-17 14:47:58 +0200

    XML

--- gyak6/gyak6/Form1.cs
+++ gyak6/gyak6/Form1.cs
@@ -6,19 +6,23 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace gyak6
 {
     public partial class Form1 : Form
     {
         new BindingList<RateData> Rates = new BindingList<RateData>();
+        public string result;
         public Form1()
         {
             InitializeComponent();
             GetExchangeRates();
+            XML();
             dataGridView1.DataSource = Rates;
         }
         private void GetExchangeRates()
@@ -31,7 +35,27 @@ namespace gyak6
                 endDate = "2020-06-30"
             };
             var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+            var eredmeny = response.GetExchangeRatesResult;
+            result = eredmeny;
+        }
+        private void XML()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                {
+                    rate.Value = value / unit;
+                }
+            }
         }
     }
 
