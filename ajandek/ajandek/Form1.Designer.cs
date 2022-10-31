f9d55cc5e8da0c6dfbafd16a85cf880c936ca5ef
zubonyai.gellert <gellert.zubonyai@stud.uni-corvinus.hu>
2022-10-31 21:53:07 +0100

    buttons

--- ajandek/ajandek/Form1.Designer.cs
+++ ajandek/ajandek/Form1.Designer.cs
@@ -32,10 +32,17 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.carbutton = new System.Windows.Forms.Button();
+            this.ballbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.ballbutton);
+            this.mainPanel.Controls.Add(this.carbutton);
             this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainPanel.Location = new System.Drawing.Point(0, 0);
             this.mainPanel.Name = "mainPanel";
@@ -52,6 +59,35 @@
             this.conveyorTimer.Enabled = true;
             this.conveyorTimer.Interval = 10;
             // 
+            // carbutton
+            // 
+            this.carbutton.Location = new System.Drawing.Point(40, 66);
+            this.carbutton.Name = "carbutton";
+            this.carbutton.Size = new System.Drawing.Size(69, 56);
+            this.carbutton.TabIndex = 0;
+            this.carbutton.Text = "CAR";
+            this.carbutton.UseVisualStyleBackColor = true;
+            this.carbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ballbutton
+            // 
+            this.ballbutton.Location = new System.Drawing.Point(115, 66);
+            this.ballbutton.Name = "ballbutton";
+            this.ballbutton.Size = new System.Drawing.Size(67, 56);
+            this.ballbutton.TabIndex = 1;
+            this.ballbutton.Text = "BALL";
+            this.ballbutton.UseVisualStyleBackColor = true;
+            this.ballbutton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(316, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coming next:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -60,6 +96,8 @@
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +107,9 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ballbutton;
+        private System.Windows.Forms.Button carbutton;
     }
 }
 
--- ajandek/ajandek/Form1.cs
+++ ajandek/ajandek/Form1.cs
@@ -51,5 +51,15 @@ namespace ajandek
                 _toys.Remove(oldestToy);
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
     }
 }
