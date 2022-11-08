5618dbbb72836be98fd04e361839743623000426
zubonyai.gellert <gellert.zubonyai@stud.uni-corvinus.hu>
2022-11-08 18:28:37 +0100

    VaR

--- week07/week07/Form1.cs
+++ week07/week07/Form1.cs
@@ -22,6 +22,24 @@ namespace week07
             Ticks = context.Tick.ToList();
             dgwTick.DataSource = Ticks;
             CreatePortfolio();
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
         }
         private void CreatePortfolio()
         {
