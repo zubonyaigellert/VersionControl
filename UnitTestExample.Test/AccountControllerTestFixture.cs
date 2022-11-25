2aceb297bbd76009a14514ff983fe2481a023f08
zubonyai.gellert <gellert.zubonyai@stud.uni-corvinus.hu>
2022-11-25 11:22:06 +0100

    Validalas teszt

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
@@ -12,7 +13,9 @@ namespace UnitTestExample.Test
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
-            
+            var accountController = new AccountController();
+            var actualResult = accountController.ValidateEmail(email);
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
