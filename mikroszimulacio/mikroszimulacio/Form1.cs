02ea7c4ba905ee846c11af3045be2cc0e7832428
zubonyai.gellert <gellert.zubonyai@stud.uni-corvinus.hu>
2022-12-02 11:53:04 +0100

    Szimuláció

--- mikroszimulacio/mikroszimulacio/Form1.cs
+++ mikroszimulacio/mikroszimulacio/Form1.cs
@@ -24,6 +24,22 @@ namespace mikroszimulacio
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év: {0} Fiúk: {1} Lányok: {2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
         public List<Person> GetPopulation(string csvpath)
         {
