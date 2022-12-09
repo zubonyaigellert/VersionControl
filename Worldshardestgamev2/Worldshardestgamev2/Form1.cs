b200e38f9768791a6a4c41cfea27feb1f0fc962b
zubonyai.gellert <gellert.zubonyai@stud.uni-corvinus.hu>
2022-12-09 14:04:48 +0100

    Induló populáció

--- Worldshardestgamev2/Worldshardestgamev2/Form1.cs
+++ Worldshardestgamev2/Worldshardestgamev2/Form1.cs
@@ -15,14 +15,21 @@ namespace Worldshardestgamev2
     {
         GameController gc = new GameController();
         GameArea ga;
-
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
-            
+           for(int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
     }
 }
