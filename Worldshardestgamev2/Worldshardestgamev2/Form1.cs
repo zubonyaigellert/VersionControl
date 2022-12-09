5a7c338e8c51f0a63bf1b80f4fca2fc346fb129f
zubonyai.gellert <gellert.zubonyai@stud.uni-corvinus.hu>
2022-12-09 14:09:30 +0100

    Iteráció

--- Worldshardestgamev2/Worldshardestgamev2/Form1.cs
+++ Worldshardestgamev2/Worldshardestgamev2/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,15 +20,43 @@ namespace Worldshardestgamev2
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
-           for(int i = 0; i < populationSize; i++)
+            gc.GameOver += Gc_GameOver;
+            for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+        }
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
             }
             gc.Start();
         }
